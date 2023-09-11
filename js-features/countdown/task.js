8993acff7e75dc22bf6cb9b5c48d811833872b6c
Alexander Kozhin <077hustler@gmail.com>
2023-09-12 00:35:10 +0300

    task 1 completed

---
+++ js-features/countdown/task.js
@@ -0,0 +1,12 @@
+const time = function () {
+  const startTimer = document.getElementById("timer");
+  let number = parseInt(startTimer.textContent, 10);
+  if (number === 0) {
+    alert("Вы победили в конкурсе");
+    startTimer.textContent = "59";
+  } else {
+    startTimer.textContent--;
+  }
+};
+setInterval(time, 1000);
+
