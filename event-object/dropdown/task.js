586c5e0c43135bb77b4c9abe0021c7ab41c950fb
Alexander Kozhin <077hustler@gmail.com>
2024-10-16 01:56:22 +0300

    all tasks ready

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,11 +1,11 @@
 const value = document.querySelector(".dropdown__value");
 const list = document.querySelector(".dropdown__list");
 const link = Array.from(document.querySelectorAll(".dropdown__link"));
-value.addEventListener("click", function () {
+value.addEventListener("click", () => {
   list.classList.toggle("dropdown__list_active");
 });
-link.forEach(e => {
-  e.addEventListener("click", function (event) {
+link.forEach((e) => {
+  e.addEventListener("click", (event) => {
     event.preventDefault();
     list.classList.remove("dropdown__list_active");
     value.textContent = event.currentTarget.textContent;
