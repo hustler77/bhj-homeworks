6775bfe4d7a926dacf22a8ff61f4d109081fd6f9
Alexander Kozhin <077hustler@gmail.com>
2024-10-21 22:43:47 +0300

    task one ready

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,10 +1,9 @@
 const reveals = Array.from(document.querySelectorAll(".reveal"));
-document.addEventListener("scroll", appearance);
-function appearance() {
+document.addEventListener("scroll", () => {
   reveals.forEach((el) => {
     const { top, bottom } = el.getBoundingClientRect();
     if (top > 0 && bottom < window.innerHeight) {
       el.classList.add("reveal_active");
     }
   });
-}
+});
