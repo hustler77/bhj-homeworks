6027d6f5312afded8e880d24786c5757316f87d8
Alexander Kozhin <077hustler@gmail.com>
2024-10-22 21:04:19 +0300

    task 2 ready

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,16 +1,1 @@
-const tabs = Array.from(document.querySelectorAll(".tab"));
-let activeTab = document.querySelector(".tab_active");
-const content = Array.from(document.querySelectorAll(".tab__content"));
-let activeContent = document.querySelector(".tab__content_active");
 
-tabs.forEach((clickedTab) => {
-  clickedTab.addEventListener("click", () => {
-    let index = tabs.indexOf(clickedTab);
-    activeTab.classList.remove("tab_active");
-    activeTab = clickedTab;
-    activeTab.classList.add("tab_active");
-    activeContent.classList.remove("tab__content_active");
-    activeContent = content[index];
-    activeContent.classList.add("tab__content_active");
-  });
-});
---
+++ let a = 5;.js
@@ -0,0 +1,7 @@
+let a = 9;
+let b = 4;
+
+function sum(a,b) {
+    return (a + b);
+}
+console.log (sum(a,b));
